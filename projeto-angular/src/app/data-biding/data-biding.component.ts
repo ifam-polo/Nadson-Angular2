c4385f8b53476d3a9374a790f2b5f7d6962572c8
nadsoncley <nadson.cley@hotmail.com>
2023-06-01 10:29:30 -0400

    event binging.

--- projeto-angular/src/app/data-biding/data-biding.component.ts
+++ projeto-angular/src/app/data-biding/data-biding.component.ts
@@ -10,10 +10,22 @@ export class DataBidingComponent {
   public nome:string = "Nadson Cley";
   public idade: number = 43;
   public CheckedDisable: boolean = false;
-  public imgSrc: string = "	https://t.ctcdn.com.br/-aepV3ltJ2nWgG-Z556MOYAmkSY=/1000x563/smart/filters:format(webp)/i257652.jpeg"
+  public imgSrc: string = "	https://t.ctcdn.com.br/-aepV3ltJ2nWgG-Z556MOYAmkSY=/1000x563/smart/filters:format(webp)/i257652.jpeg";
+  public imgTitle: string ="Property Binding";
+  public position: {x: number, y:number} = {x:0, y:0};
   
   constructor(){}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent){
+      console.log(valor);
   }
+
+  public mouseMoveTeste(valor: MouseEvent){
+  
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
+
 }
